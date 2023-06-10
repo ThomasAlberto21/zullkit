808eee293d25f79f48e664ce3baf23fa5d01a3f2
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-10 20:43:04 +0700

    .

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,24 +1,21 @@
-<template>
- <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-  <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
-   <Logo />
-   <UserInfo />
-   <NavigationLinks />
-  </div>
- </nav>
-</template>
-
-<script>
+<script setup>
 import Logo from './logo.vue'
 import NavigationLinks from './NavigationLinks.vue'
 import UserInfo from './UserInfo.vue'
+</script>
 
+<template>
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
+      <Logo />
+      <UserInfo />
+      <NavigationLinks />
+    </div>
+  </nav>
+</template>
+
+<script>
 export default {
- name: 'NavbarComponent',
- components: {
-  Logo,
-  NavigationLinks,
-  UserInfo
- }
+  name: 'NavbarComponent'
 }
 </script>
