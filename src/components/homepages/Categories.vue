4d3f7ceefc86bf22120f7b18ec597c743754325a
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-10 20:20:52 +0700

    .

--- src/components/homepages/Categories.vue
+++ src/components/homepages/Categories.vue
@@ -2,10 +2,10 @@
   <div class="container px-4 mx-auto my-16 md:px-12">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      <CategoriesCard title="Mobile UI Kit" count="731" image="categories-1.jpg" />
-      <CategoriesCard title="Fonts" count="657" image="categories-2.jpg" />
-      <CategoriesCard title="Icon Set" count="83,599" image="categories-3.jpg" />
-      <CategoriesCard title="Website UI Kit" count="4,500" image="categories-4.jpg" />
+      <CategoriesCard title="Mobile UI Kit" :count="731" image="categories-1.jpg" />
+      <CategoriesCard title="Fonts" :count="657" image="categories-2.jpg" />
+      <CategoriesCard title="Icon Set" :count="83.599" image="categories-3.jpg" />
+      <CategoriesCard title="Website UI Kit" :count="4.500" image="categories-4.jpg" />
     </div>
   </div>
 </template>
